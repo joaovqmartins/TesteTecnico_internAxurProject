c758227da3f7b9ef3455a4c1d051073548f30f6b
joaovqmartins <joavmartin3zp1n@gmail.com>
2025-02-16 01:03:09 -0300

    Atualizando projeto

--- HtmlAnalyzer.java
+++ HtmlAnalyzer.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.util.Stack;
 
 public class HtmlAnalyzer {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("HtmlAnalyzer iniciado.");
 
         if (args.length != 1) {
-            System.out.println("Uso correto: java HtmlAnalyzer <URL>");
+            System.out.println("Jeito certo de usar: java HtmlAnalyzer <URL>");
             return;
         }
 
@@ -23,12 +25,15 @@ public class HtmlAnalyzer {
         }
 
         System.out.println("Conteúdo HTML obtido:\n" + htmlContent);
+
+        String deepsestText = extractDeepestText(htmlContent);
+        System.out.println(deepsestText);
     }
 
     // crinado o métode para baixar o html da url
     private static String fetchHtml(String urlString) {
         try {
-            URL url = new URL(urlString);
+            URL url = new URI(urlString).toURL(); // resolvendo url deprecated
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
@@ -43,12 +48,45 @@ public class HtmlAnalyzer {
             while ((line = reader.readLine()) != null) {
                 content.append(line).append("\n");
             }
-            reader .close();
+            reader.close();
 
             return content.toString();
 
         } catch (Exception e) {
             return null;
         }
+    }
+
+    private static String extractDeepestText(String html) {
+        Stack<String> tagStack = new Stack<>();
+        String deepestestText = "";
+        int maxDepth = 0;  // maxima profundiadade
+        int currentDepth = 0; //atual profundidade
+
+        String[] lines = html.split("\n");
+        for (String line : lines) {
+            line = line.trim();
+            if (line.isEmpty()) continue;
+
+            if (line.startsWith("<") && line.endsWith(">")) {
+                if (line.startsWith("</")) {
+                    if (!tagStack.isEmpty()) {
+                        tagStack.pop();
+                        currentDepth--; //dimunui a profundidade atual em 1
+                    } else {
+                        return "malformed HTML";
+                    }
+                } else {
+                    tagStack.push(line);
+                    currentDepth++;
+                }
+            } else {
+                if (currentDepth > maxDepth) {
+                    maxDepth = currentDepth;
+                    deepestestText = line;
+                }
+            }
+        }
+        return tagStack.isEmpty() ? deepestestText : "malformed HTML";
     }
 }
